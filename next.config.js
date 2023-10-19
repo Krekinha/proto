26fe83479d6dcc59660c43f9c27fdf8470ed2f0c
Dionison <krekmg@gmail.com>
2023-10-19 06:57:34 -0300

    45

--- next.config.js
+++ next.config.js
@@ -1,35 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-const securityHeaders = [
-  {
-    key: "X-Frame-Options",
-    value: "SAMEORIGIN",
-  },
-];
-
-const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
-        port: "",
-        pathname: "/**",
-      },
-    ],
-  },
-  async headers() {
-    return [
-      {
-        // Apply these headers to all routes in your application.
-        source: "/:path*",
-        headers: securityHeaders,
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
