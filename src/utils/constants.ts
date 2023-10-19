9b7fbf77dc4a76733bfbbeb1175a6aa52f7d0e12
Dionison <krekmg@gmail.com>
2023-10-19 08:40:18 -0300

    prot

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export function baseUrl() {
-  if (process.env.NODE_ENV === "production") return "https://makit.vercel.app";
+  if (process.env.NODE_ENV === "production") return "https://proto-ochre.vercel.app";
   else return "http://localhost:3000";
 }
 
