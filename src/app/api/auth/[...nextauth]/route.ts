e8b6785dec14c43a247aa127b9cc72e789be32c0
Dionison <krekmg@gmail.com>
2023-10-20 03:36:30 -0300

    22

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -11,7 +11,7 @@ const handler = NextAuth({
   secret: process.env.NEXTAUTH_SECRET,
   providers: [
     CredentialsProvider({
-      name: "credentials",
+      name: "Credentials",
       credentials: {
         email: {},
         senha: {},
